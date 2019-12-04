960bd131151d8b55353a914f750cbba78e89b4c5
John Ruble <john.ruble@atomicobject.com>
2019-12-04 10:59:54 -0500

    solve 4a

--- 04/Solve.fs
+++ 04/Solve.fs
@@ -10,6 +10,31 @@ let seqFrom ((a,b):int*int) =
             yield i
     }
     
+    (*
+    The value is within the range given in your puzzle input.
+    It is a six-digit number.
+    Two adjacent digits are the same (like 22 in 122345).
+    Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+    *)
+    
+let lengthSix s = (Seq.length s) = 6
+
+
+
+let hasAdjacentMatchingDigits (s:string) =
+    s |> Seq.pairwise |> Seq.tryFind (fun (a,b) -> a = b) |> Option.isSome
+
+let monotonicallyIncreasing (s:string) =
+    s |> Seq.pairwise |> Seq.tryFind (fun (a,b) -> a > b) |> Option.isNone
+
 let solve (input:string) =
-    true
+    input
+        |> parse
+        |> seqFrom
+        |> Seq.map (fun s -> s.ToString())
+        |> Seq.where lengthSix
+        |> Seq.where hasAdjacentMatchingDigits
+        |> Seq.where monotonicallyIncreasing
+        |> Seq.length
+   
 
--- 04/Tests.fs
+++ 04/Tests.fs
@@ -12,3 +12,25 @@ let ``parse`` () =
 [<Fact>]
 let ``seqFrom`` () =
     seqFrom (23,25) |> Seq.toList |> should equal [23;24;25]
+    
+    
+[<Fact>]
+let ``hasAdjacentMatchingDigits false`` () =
+    hasAdjacentMatchingDigits "1234" |> should equal false
+    
+[<Fact>]
+let ``hasAdjacentMatchingDigits true`` () =
+    hasAdjacentMatchingDigits "12334" |> should equal true
+    
+[<Fact>]
+let ``monotonicallyIncreasing true`` () =
+    monotonicallyIncreasing "12225" |> should equal true
+    
+[<Fact>]
+let ``monotonicallyIncreasing false`` () =
+    monotonicallyIncreasing "12315" |> should equal false
+    
+
+[<Fact>]
+let ``solves problem`` () =
+    solve "108457-562041" |> should equal 2779
