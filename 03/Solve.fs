8a97f27ed5c8fb101a91149f5370469f44b393c2
John Ruble <john.ruble@atomicobject.com>
2019-12-04 02:00:20 -0500

    solve part 2

--- 03/Solve.fs
+++ 03/Solve.fs
@@ -37,12 +37,6 @@ let manhattan (input:int*int) =
     (input |> fst |> abs) + (input |> snd |> abs)
     
 let solve (input:string) =
-    let lines = input.Split('\n')
-    assert (lines.Length = 2)
-    let points1 = lines.[0] |> expandPath |> pointsForPath |> Set.ofSeq
-    let points2 = lines.[1] |> expandPath |> pointsForPath |> Set.ofSeq
-    let intersection = Set.intersect points1 points2
-    intersection |> Set.toSeq |> Seq.sortBy manhattan |> Seq.head |> manhattan
     
     (*
     approach:
@@ -53,6 +47,38 @@ let solve (input:string) =
     
     find first point that exists in both sets
     *)
-    
-    
 
+    let lines = input.Split('\n')
+    assert (lines.Length = 2)
+    let points1 = lines.[0] |> expandPath |> pointsForPath |> Set.ofSeq
+    let points2 = lines.[1] |> expandPath |> pointsForPath |> Set.ofSeq
+    let intersection = Set.intersect points1 points2
+    intersection |> Set.toSeq |> Seq.sortBy manhattan |> Seq.head |> manhattan
+    
+let countingNumbers () = Seq.initInfinite id |> Seq.skip 1
+
+type WireCoord = { wireLen:int; pos:int*int;which:char;}
+let processGroup (g:WireCoord seq) =
+    let firstWirePoints = g |> Seq.where (fun w -> w.which = 'A')
+    let secondWirePoints = g |> Seq.where (fun w -> w.which = 'B')
+    if firstWirePoints |> Seq.length = 0 then
+        None
+    else if secondWirePoints |> Seq.length = 0 then
+        None
+    else
+        let shortestA = firstWirePoints |> Seq.map (fun w -> w.wireLen) |> Seq.sort |> Seq.head
+        let shortestB = secondWirePoints |> Seq.map (fun w -> w.wireLen) |> Seq.sort |> Seq.head
+//        sprintf "group len %d" (g |> Seq.length) |> Some
+        shortestA + shortestB |> Some
+let solve2 (input:string) =
+    let lines = input.Split('\n')
+    assert (lines.Length = 2)
+//    let counting = Seq.initInfinite id |> Seq.skip 1
+
+    let points1 = lines.[0] |> expandPath |> pointsForPath |> Seq.mapi (fun i s -> {wireLen=i;pos=s;which='A'})
+    let points2 = lines.[1] |> expandPath |> pointsForPath |> Seq.mapi  (fun i s -> {wireLen=i;pos=s;which='B'})
+    let allPoints = Seq.append points1 points2
+    let groups = allPoints |> Seq.groupBy (fun c -> c.pos) |> Seq.map snd
+    
+    (groups |> Seq.map processGroup |> List.ofSeq |> List.choose id |> List.sort |> List.head) + 2
+    
