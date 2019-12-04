c2e215d06292b50d183594729bacadc36788a273
John Ruble <john.ruble@atomicobject.com>
2019-12-03 22:53:47 -0500

    fsunit and gitignore

--- 03/Tests.fs
+++ 03/Tests.fs
@@ -2,7 +2,12 @@ module Tests
 
 open System
 open Xunit
+open FsUnit.Xunit
+
+let foo x =
+    x + 5
 
 [<Fact>]
 let ``My test`` () =
-    Assert.True(true)
+    true |> should equal true
+    foo 5 |> should equal 10
