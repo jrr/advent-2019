88c2b94c79fbf81431be9d8321b63f7dd72dae2b
John Ruble <john.ruble@atomicobject.com>
2019-12-06 22:59:19 -0500

    6a

--- 06/Solve.fs
+++ 06/Solve.fs
@@ -1,5 +1,36 @@
 module Solve
 open System
+        
+let buildMap (input:string) =
+    input.Split('\n')
+        |> Seq.ofArray
+        |> Seq.map (fun s ->
+            let [|a;b|] = s.Split(')')
+            (a,b)
+            )
+        |> Seq.groupBy fst
+        |> Seq.map (fun (x, y) -> (x, Seq.map snd y))
+        |> Map.ofSeq
+        
+type WalkResult = {nodes:int;orbits:int}
 
+let addWalkResults =
+    (fun r s -> {nodes=r.nodes+s.nodes;orbits=r.orbits+s.orbits})
+    
+let rec walk (map: Map<string,string seq>) (node:string) (depth:int)=
+    let children = map |> Map.tryFind node
+    
+    let childrenSum = match children with
+                        | None -> {nodes=0;orbits=0} // if the node name isn't found, then it is a leaf without children
+                        | Some ch ->
+                            let results = ch |> Seq.map (fun c -> walk map c (depth+1))
+                            let sum = results |> Seq.fold addWalkResults {nodes=0; orbits=0}
+                            sum
+    printfn "node %s sum %s" node (childrenSum.ToString())
+    addWalkResults childrenSum {nodes=1;orbits=childrenSum.nodes}
+    
 let solve (input:string) =
-    input
+    let map = buildMap input
+//    let lookup = map |> Map.find "COM"
+//    printfn "map %s" (map.ToString())
+    (walk map "COM" 0).orbits
--- 06/Tests.fs
+++ 06/Tests.fs
@@ -1,11 +1,65 @@
 module Tests
 
 open System
+open System.Xml.Linq
 open Xunit
 open FsUnit.Xunit
 open Solve
 
 [<Fact>]
-let ``solves problem`` () =
-    solve "foo" |> should equal "foo"
+let ``solves example`` () =
+    solve Test_Inputs.puzzle6example |> should equal 42
     
+[<Fact>]
+let ``solves 6A`` () =
+    solve Test_Inputs.puzzle6input |> should equal 253104
+    
+[<Fact>]
+let ``walk single node`` () =
+    let map = Map.empty.Add ("COM",Seq.empty)
+    walk map "COM" 0 |> should equal {nodes=1; orbits=0}
+    
+[<Fact>]
+let ``walk two-node`` () =
+    (*    COM -- A -- B    *)
+    let input = "COM)A"
+    let map = buildMap input
+    walk map "COM" 0 |> should equal {nodes=2; orbits=1}
+    
+[<Fact>]
+let ``walk three-node linear`` () =
+    (*    COM -- A -- B    *)
+    let input = "COM)A
+A)B"
+    let map = buildMap input
+    walk map "COM" 0 |> should equal {nodes=3; orbits=3}
+    
+[<Fact>]
+let ``walk three-node fork`` () =
+    (*    COM -- A
+              \
+                B     *)
+    let input = "COM)A
+COM)B"
+    let map = buildMap input
+    walk map "COM" 0 |> should equal {nodes=3; orbits=2}
+    
+[<Fact>]
+let ``walk four-node linear`` () =
+    (*    COM -- A -- B -- C   *)
+    let input = "COM)A
+A)B
+B)C"
+    let map = buildMap input
+    walk map "COM" 0 |> should equal {nodes=4; orbits=6}
+    
+[<Fact>]
+let ``walk four-node fork`` () =
+    (*    COM -- A -- B
+                   \
+                     C      *)
+    let input = "COM)A
+A)B
+A)C"
+    let map = buildMap input
+    walk map "COM" 0 |> should equal {nodes=4; orbits=5}
