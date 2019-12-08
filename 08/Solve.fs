1d596bb49a93ae0efb7a61c7c8a8dc8a56a569f4
John Ruble <john.ruble@atomicobject.com>
2019-12-08 01:44:54 -0500

    8b

--- 08/Solve.fs
+++ 08/Solve.fs
@@ -14,3 +14,28 @@ let solve (input:string) =
     let numberOfOnes = groupWithFewestZeroes |> Seq.filter (fun c -> c = '1') |> Seq.length
     let numberOfTwos = groupWithFewestZeroes |> Seq.filter (fun c -> c = '2') |> Seq.length
     numberOfOnes * numberOfTwos
+
+let addLayer ((a,b):char*char) =
+    match (a,b) with
+//    | ('0','0') -> '0'
+//    | ('1','1') -> '1'
+//    | ('2','2') -> '2'
+    | (x,'2') -> x // transparent
+    | (_,color) -> color
+    | _ -> failwith "oops"
+    
+let fn (a:char[]) (b:char[]) =
+    Array.zip a b |> Seq.map addLayer |> Array.ofSeq
+let solve8b (input:string) =
+    let groups = input |> Seq.splitInto 100 |> Seq.rev // "at most" this many chunks
+      
+    // assert it actually gave us 100
+    assert (groups |> Seq.length = 100)
+    
+    // and that the groups are evenly sized
+    assert (groups |> Seq.map (fun g -> g.Length) |> Seq.distinct  |> Seq.length  = 1)
+    
+    let firstLayer = groups |> Seq.head
+    let remainingLayers = groups |> Seq.skip 1
+    let result = remainingLayers |> Seq.fold fn firstLayer  |> System.String
+    result
--- 08/Tests.fs
+++ 08/Tests.fs
@@ -8,5 +8,17 @@ open Solve
 [<Fact>]
 let ``solves problem`` () =
     solve Test_Input.givenInput |> should equal 2176
+
     
 
+[<Fact>]
+let ``solves 8b`` () =
+    solve8b Test_Input.givenInput |> should equal ""
+    (*
+ XX  X   XX  X XXX  X   X
+X  X X   XX X  X  X X   X
+X     X X XX   XXX   X X 
+X      X  X X  X  X   X  
+X  X   X  X X  X  X   X  
+ XX    X  X  X XXX    X  
+    *)
