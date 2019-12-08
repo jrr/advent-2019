7980f8550b371f1622cd571101ed2dc85eb79483
John Ruble <john.ruble@atomicobject.com>
2019-12-08 01:50:09 -0500

    fix

--- 08/Tests.fs
+++ 08/Tests.fs
@@ -13,7 +13,8 @@ let ``solves problem`` () =
 
 [<Fact>]
 let ``solves 8b`` () =
-    solve8b Test_Input.givenInput |> should equal ""
+    solve8b Test_Input.givenInput |> should equal "011001000110010111001000110010100011010010010100011000001010110001110001010100000010010100100100010010010001001010010010001000110000100100101110000100"
+    
     (*
  XX  X   XX  X XXX  X   X
 X  X X   XX X  X  X X   X
